fcc51ae8ccc27b5faabe0bfd3def81975ab90865
Feri Lukmansyah <47948861+perymerdeka@users.noreply.github.com>
2024-08-09 15:29:05 +0700

    Updated Client

--- src/config/firebaseClient.js
+++ src/config/firebaseClient.js
@@ -1,4 +1,5 @@
 // Firebase Client SDK (Client-side)
+const { cert } = require("firebase-admin/app");
 const { initializeApp } = require("firebase/app");
 const { 
   getAuth, 
@@ -9,10 +10,12 @@ const {
   sendPasswordResetEmail 
 } = require("firebase/auth");
 
+const serviceAccount = require("../../serviceAccountKey.json");
+
 const firebaseConfig = {
-    apiKey: process.env.FIREBASE_API_KEY,
-    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-    projectId: process.env.FIREBASE_PROJECT_ID,
+    apiKey: process.env.FIREBASE_API_KEY || "AIzaSyD0KHqJ8BNEKvkjN93njGoeopyGXdHYuwk",
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN || "expressblog-534ef.firebaseapp.com",
+    projectId: process.env.FIREBASE_PROJECT_ID || "expressblog-534ef",
     storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
     messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
     appId: process.env.FIREBASE_APP_ID
